6ea804cbed6b15709fadbf2cd4f124c81c51306f
Gaurav Goswami <gouravgoswami48@gmail.com>
2021-06-09 16:29:02 +0530

    fix(Action): second drop

---
+++ build/setupSR.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -euo pipefail
+
+nvm install 12
+npm install -g npm@6.x
+sudo apt-get update
+sudo apt-get install python
+pip install --user bumpversion
+npm install @semantic-release/changelog
+npm install @semantic-release/exec
+npm install @semantic-release/git
+npm install @semantic-release/github
